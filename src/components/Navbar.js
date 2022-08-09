5b46fae5f8b45e300315e9f896cab2c596daead4
Jabbernaut <jabbernaut4@gmail.com>
2022-08-08 22:30:07 -0400

    push 4

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -10,9 +10,9 @@ const Navbar = () => {
         <div className="container">
             <a href="/" className='brand-logo'>PokeTimes</a>
             <ul className="right">
-                <li><a href="/">Home</a></li>
-                <li><a href="/about">About</a></li>
-                <li><a href="/contact">Contact</a></li>
+                <li><a href="./">Home</a></li>
+                <li><a href="./about">About</a></li>
+                <li><a href="./contact">Contact</a></li>
             </ul>
         </div>
     </nav>
