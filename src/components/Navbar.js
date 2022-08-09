08c98e93f505b04dd42776d2b6cbf39ad8a9dd55
Jabbernaut <jabbernaut4@gmail.com>
2022-08-08 22:20:02 -0400

    push 3

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -2,13 +2,13 @@ import React from 'react'
 
 const Navbar = () => {
     // setTimeout(() => {
-    // props.history.push
-    // });
+    // props.history.push('/about')
+    // }, 2000);
 
   return (
     <nav className='nav-wrapper red darken-3'>
         <div className="container">
-            <a href="_" className='brand-logo'>PokeTimes</a>
+            <a href="/" className='brand-logo'>PokeTimes</a>
             <ul className="right">
                 <li><a href="/">Home</a></li>
                 <li><a href="/about">About</a></li>
